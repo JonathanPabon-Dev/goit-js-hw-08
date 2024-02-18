ea317bd818c6076f9d051a018b231dcb4873fd98
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-02-18 11:44:43 -0500

    Tarea 3 finalizada

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,52 +1,60 @@
 import throttle from 'lodash.throttle';
 
-const form = document
-  .querySelector('.feedback-form')
-  .addEventListener('submit', submitForm)
-  .addEventListener('input', getFormData);
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = document.querySelector('input[name="email"]');
+  const messageTextarea = document.querySelector('textarea[name="message"]');
 
-const emailInput = document.querySelector('input[name="email"]');
-const messageTextarea = document.querySelector('textarea[name="message"]');
+  let formData = {
+    email: '',
+    message: '',
+  };
 
-const formObj = {
-  email: '',
-  message: '',
+  let formState = localStorage.getItem('feedback-form-state');
+  if (formState) {
+    fillForm(JSON.parse(formState));
+  }
 
-  getEmail() {
-    return this.email;
-  },
-  getMessage() {
-    return this.message;
-  },
-  setEmail(newEmail) {
-    this.email = newEmail;
-  },
-  setMessage(newMessage) {
-    this.message = newMessage;
-  },
-};
+  function fillForm(dataForm) {
+    emailInput.value = dataForm.email;
+    messageTextarea.value = dataForm.message;
+  }
 
-let formState = localStorage.getItem('feedback-form-state');
-if (formState) {
-  localStorage.setItem('feedback-form-state', JSON.stringify(formObj));
-}
+  const getFormData = throttle(function () {
+    formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500);
 
-function getFormData() {
-  const emailValue = emailInput.value;
-  const messageValue = messageTextarea.value;
+  form.addEventListener('input', getFormData);
 
-  console.log(`Email: ${emailValue}`);
-  console.log(`Message: ${messageValue}`);
-  formObj.setEmail(JSON.parse(formState).email);
-  formObj.setMessage(JSON.parse(formState).message);
-}
+  form.addEventListener('submit', e => {
+    formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+    if (formData.email && formData.message) {
+      submitForm(e);
+    } else {
+      window.alert('Fill all fields.');
+    }
+  });
 
-function submitForm() {
-  console.log(formObj);
-  clearForm();
-}
+  function submitForm(e) {
+    e.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    console.log(formData);
+    clearForm();
+  }
 
-function clearForm() {
-  emailInput.value = '';
-  messageTextarea.value = '';
-}
+  function clearForm() {
+    form.reset();
+    emailInput.focus();
+    formData = {
+      email: '',
+      message: '',
+    };
+  }
+});
